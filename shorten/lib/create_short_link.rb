2b1dedf597ca72ac8dae640df10668ee52d38a3f
Alexander Alemayhu <alexander@alemayhu.com>
2022-01-16 12:16:31 +0100

    shorten: add retry logic when a collision occurs

--- shorten/lib/create_short_link.rb
+++ shorten/lib/create_short_link.rb
@@ -1,10 +1,23 @@
 require 'securerandom'
 
+LENGTH = 6
 class CreateShortLink
   attr_accessor :url, :short_link
 
   def initialize(url:)
     @url = url
-    @short_link = SecureRandom.urlsafe_base64(6)
+    @short_link = SecureRandom.urlsafe_base64(LENGTH)
+  end
+
+  # In case of a collision attempt to recreate upto 10 times
+  def recreate_on_collision(&does_collide)
+    new_link = @short_link
+    10.times do
+      new_link = SecureRandom.urlsafe_base64(LENGTH)
+      unless does_collide.call(new_link)
+        @short_link = new_link
+        break
+      end
+    end
   end
 end
--- shorten/spec/create_short_link.rb
+++ shorten/spec/create_short_link.rb
@@ -5,4 +5,12 @@ RSpec.describe CreateShortLink do
     link = CreateShortLink.new(url: 'http://www.google.com')
     expect(link.short_link.length).to be 8
   end
+
+  it 'should create new short link on retry' do
+    link = CreateShortLink.new(url: 'http://www.wikipedia.com')
+    first_short_link = link.short_link
+    link.recreate_on_collision { |short_link| short_link == first_short_link }
+    new_short_link = link.short_link
+    expect(new_short_link).not_to eq first_short_link
+  end
 end
